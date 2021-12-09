e1c1ec7e65c50e11a79e69fd75ce6b5dac193b0f
David Hoang <dhoang@aais.com>
2021-12-09 16:58:27 -0600

    preparing for frontend with CORS

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -7,7 +7,20 @@ export default class StorageStack extends sst.Stack {
     table;
     constructor(scope, id, props) {
         super(scope, id, props);
-        this.bucket = new sst.Bucket(this, "Uploads")
+        //prepare for CORS in uploads
+        //this.bucket = new sst.Bucket(this, "Uploads")
+        this.bucket = new sst.Bucket(this, "Uploads", {
+            s3Bucket: {
+                cors: [
+                    {
+                        maxAge: 3000,
+                        allowedOrigins: ["8"],
+                        allowedHeaders: ["*"],
+                        allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+                    },
+                ],
+            },
+        });
         this.table = new sst.Table(this, "Notes", {
             fields: {
                 userId: sst.TableFieldType.STRING,
