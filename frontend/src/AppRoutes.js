d8ecd46ab75757f2aa01c7a0b6ce761e014a7774
David Hoang <dhoang@aais.com>
2021-12-10 15:35:37 -0600

    login functional

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
 
 
 const AppRoutes = () => {
@@ -9,6 +10,7 @@ const AppRoutes = () => {
             <Routes>
                 <Route path="/" element={<Home />}></Route>
                 <Route path="*" element={<NotFound/>}></Route>
+                <Route path="/login" element={<Login/>}></Route>
             </Routes>
     );
 }
